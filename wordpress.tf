e80e1d36dbb7ccbec4be695cbd01e662f055d4d2
aniketambasta <64227238+aniketambasta@users.noreply.github.com>
2020-09-25 20:58:49 +0530

    wp

---
+++ wordpress.tf
@@ -0,0 +1,62 @@
+provider "kubernetes" {
+    config_context_cluster   = "minikube"
+}
+resource "kubernetes_deployment" "anipress" {
+  metadata {
+    name = "wordpress"
+  }
+  spec {
+    replicas = 3    
+    selector{
+    match_labels = {
+      env = "Development"
+      dc = "IN"
+      App = "wordpress"
+    }
+    match_expressions {
+      key = "env"
+      operator = "In"
+      values = ["Development" , "wordpress"]
+    }
+  }
+   template {
+        metadata {
+         labels = {
+      env = "Development"
+      dc = "IN"
+      App = "wordpress"
+    }
+        }
+
+
+
+
+      spec {
+        container {
+          image = "wordpress:4.8-apache"
+          name  = "anios"
+
+
+
+
+        }
+      }
+    }
+}
+}
+resource "kubernetes_service" "anilb" {
+  metadata {
+    name = "loadbalancer"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.anipress.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      node_port   = 30000 
+      port        = 80
+      target_port = 80
+    }
+    type = "NodePort"
+} 
+}
